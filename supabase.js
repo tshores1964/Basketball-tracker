9f289d09bf466ef69fd852d1abe2be323744a6e3
tshores1964 <tshores@platinumcandf.com>
2026-05-18 20:00:44 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,15 @@
 const SUPABASE_URL = "https://zpetypwibsyjdserzhnu.supabase.co";
-const SUPABASE_KEY = "sb_publishable_eyUIqKiJmPyQ7athEq9LcA_b1AHdSOY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwZXR5cHdpYnN5amRzZXJ6aG51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMzgzNzcsImV4cCI6MjA5NDcxNDM3N30.xIoVqgBeHYLLXEHJ__ONijx-eNGyFh08CDrkukBs050";
 
-const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
+  global: {
+    headers: {
+      'X-Client-Info': 'basketball-tracker'
+    }
+  },
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+    detectSessionInUrl: false
+  }
+});
